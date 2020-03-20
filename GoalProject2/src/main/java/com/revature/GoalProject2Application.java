4efcf14ae5f79a7cc91d20c347efa1503d8e1b41
Anthony-Cona <55373384+Anthony-Cona@users.noreply.github.com>
2020-03-20 11:34:26 -0400

    Add files via upload

---
+++ GoalProject2/src/main/java/com/revature/GoalProject2Application.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalProject2Application.class, args);
+	}
+
+}
