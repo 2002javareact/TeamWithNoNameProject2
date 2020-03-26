07714eb405e63b72f61ef3196f34a0736b70ac85
Anthony-Cona <acon5000@gmail.com>
2020-03-26 19:59:22 -0400

    Added endpoint for find goals by UserId

--- GoalProject2/src/main/java/com/revature/daos/GoalsDao.java
+++ GoalProject2/src/main/java/com/revature/daos/GoalsDao.java
@@ -4,6 +4,7 @@ package com.revature.daos;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.revature.models.Goals;
@@ -13,6 +14,7 @@ import com.revature.models.Users;
 
 public interface GoalsDao extends JpaRepository<Goals,Integer> {
 
-	public List<Goals> findByUserId(int id);
+	@Query("select g from Goals g where g.user.userId = ?1")
+	List<Goals> findByUserId(int id);
 	
 }
